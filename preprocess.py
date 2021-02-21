27eecf4575661cc5c3cd2f61d6de1b1410388a08
lonelyprince7 <1056692290@qq.com>
2021-02-21 17:40:48 +0800

    add

--- preprocess.py
+++ preprocess.py
@@ -2,83 +2,38 @@
 Descripttion: 
 Version: 1.0
 Author: ZhangHongYu
-Date: 2021-02-18 13:15:08
+Date: 2021-02-18 13:18:01
 LastEditors: ZhangHongYu
-LastEditTime: 2021-02-21 01:03:06
+LastEditTime: 2021-02-21 17:40:32
 '''
-import pandas as pd
+from preprocess import read_data
+from preprocess import features_eng
+from sklearn.model_selection import train_test_split
+from stacking import train_model
+from stacking import evaluate_model
 import numpy as np
-import os
-from sklearn import preprocessing
-
-# 数据存放目录定义
-data_root = '/home/macong/project/A题全部数据/'
-
-def read_data():
-    data1 = pd.read_csv(data_root+'基础数据.csv', encoding='GB2312')
-    data2 = pd.read_csv(data_root+'年数据.csv', encoding='GB2312')
-    # print(data2)
-    # reader3 = pd.read_table(
-    #     os.path.join(data_root, '日数据.csv'),
-    #     encoding='GB2312',
-    #     sep=',',
-    #     iterator=True)
-    # chunks = []
-    # loop = True
-    # chunkSize = 10000
-    # while loop:
-    #     try:
-    #         chunk = reader3.get_chunk(chunkSize)
-    #         print("**********")
-    #         print(chunk)
-    #         print("*********")
-    #         chunks.append(chunk)
-    #     except StopIteration:
-    #         loop = False
-    #         print("Iteration is stopped.")
-    # data = pd.concat(chunks, ignore_index=True)
-    # 结合基本数据和年数据，暂时不考虑日数据
-    combined_data = pd.merge(data2, data1, how="outer", on="股票编号")
-    labels = combined_data['是否高转送'].to_list()
-    # 我们根据上一年的特征预测下一年是否高送转，故标签是下一年的
-    for i in range(len(labels)-1):
-        labels[i] = labels[i+1]
-    combined_data['是否高转送'] = pd.Series(labels)
-    return combined_data
+import pandas as pd
 
 
-def features_eng(data):
-    #  获得每个特征的缺失信息
-    null_info = data.isnull().sum(axis=0)
-    #  丢弃缺失值多于30%的特征
-    features = [k for k, v in dict(null_info).items() if v < data.shape[0]* 0.3]
-    data = data[features]
-    null_info = data.isnull().sum(axis=0)
+if __name__ == '__main__':
 
-    # 选去出需要填补缺失值的特征
-    features_fillna = [k for k, v in dict(null_info).items() if v > 0]
-    # 缺失值填充，将列按出现频率由高到低排序，众数即第一行，inplace表示原地修改
+    data = read_data()
 
-    # 用众数对缺失值进行填补
-    data.loc[:,  features_fillna] = data[features_fillna].fillna(
-        data[features_fillna].mode().iloc[0]
-    )
+    data = features_eng(data)
 
-    # 字符独热编码，数值归一化
-    for col in data.columns:
-        if col == '是否高转送':  # 跳过标签列
-            continue
-        if str(data[col].dtype) == 'object':
-            # 字符->数值
-            data.loc[:, col] = pd.factorize(
-                data[col])[0]
-            # 获取one-hot编码
-            dummies_df = pd.get_dummies(data[col], prefix=str(col))
-            data = data.drop(col, axis=1)
-            data = data.join(dummies_df)
-        else:
-            # 对数值特征归一化
-            scaler = preprocessing.StandardScaler().fit(
-                np.array(data[col]).reshape(-1, 1))
-            data.loc[:,  col] = scaler.transform(np.array(data[col]).reshape(-1, 1))
-    return data
+    
+    submission_X = data[data['年份（年末）'] == 7].drop('是否高转送', axis=1)
+
+    my_data = data[data['年份（年末）'] != 7]
+    
+    y  = np.array(my_data[ '是否高转送'])
+    X = np.array(my_data.drop('是否高转送', axis=1))
+
+    # 划分训练集和测试集
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4)
+
+    # 采用交叉验证法对初级学习器和次级学习器进行训练
+    train_model(X_train, y_train)
+
+    # 评估学习器的性能
+    evaluate_model(X_test, y_test)
--- stacking.py
+++ stacking.py
@@ -4,7 +4,7 @@ Version: 1.0
 Author: ZhangHongYu
 Date: 2021-02-18 13:11:38
 LastEditors: ZhangHongYu
-LastEditTime: 2021-02-21 01:02:45
+LastEditTime: 2021-02-21 17:40:03
 '''
 from numpy import array
 import numpy as np
@@ -18,10 +18,12 @@ from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.tree import DecisionTreeClassifier
 from xgboost import XGBClassifier
 from sklearn.linear_model import LogisticRegression
+from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import recall_score
 from sklearn.metrics import precision_score
-from sklearn.metrics import roc_auc_score as AUC
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import f1_score
 from sklearn.model_selection import RandomizedSearchCV
 from imblearn.over_sampling import SMOTE
 from sklearn.metrics import precision_recall_curve
@@ -35,50 +37,57 @@ k = 2  # 交叉验证折数
 
 #  基分类器和次级分类器定义，都是二分类，且定义网格超参数搜索
 #  基分类器定义
-models = {}
-models.update({'dt': DecisionTreeClassifier(max_depth=8)})
-# models.update({'rf':
-#     RandomForestClassifier(n_estimators=500, warm_start=True, max_features='sqrt', max_depth=6,
-#     min_samples_split=3, min_samples_leaf=2, n_jobs=-1, verbose=0)
+models={}
+
+models.update({'dt': DecisionTreeClassifier(random_state=0)})
+models.update({'rf': 
+    RandomForestClassifier(random_state=0)
+})
+models.update({'knn': KNeighborsClassifier()})
+models.update({'et': 
+    ExtraTreesClassifier(random_state=0)
+})
+# models.update({'mlp': 
+#     MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(15,), random_state=1)
 # })
-models.update({'knn': KNeighborsClassifier(n_neighbors=3)})
-# models.update({'et':
-#     ExtraTreesClassifier(n_estimators=500, n_jobs=-1, max_depth=8, min_samples_leaf=2, verbose=0)
-# })
+
 
 # 基分类器超参数搜索范围定义
 param_grids = {}
 param_grids.update({
     'dt':
-    {'min_samples_split': [2, 4], 'max_depth': [20]}
+    { 'min_samples_split': [2, 4], 'max_depth': [4]}
 })
-# param_grids.update({
-#     'rf':
-#     {'n_estimators': [500], 'min_samples_split': [2, 3], 'max_depth': [20]}
-# })
+param_grids.update({
+    'rf':
+    {'n_estimators': [500], 'min_samples_split': [2, 3], 'max_depth': [4]}
+})
 param_grids.update({
     'knn':
-    {'n_neighbors': [i+1 for i in range(10)]}
+    {'n_neighbors':[ 10] }
 })
+param_grids.update({
+    'et':
+   {'n_estimators': [500], 'min_samples_split': [3, 4], 'max_depth': [4]}
+})
+
 # param_grids.update({
-#     'et':
-#    {'n_estimators': [500], 'min_samples_split': [3, 4], 'max_depth': [20]}
+#     'mlp':{}
 # })
 
-#  完成超参数网格搜索后的模型
-model_grids = {}
+
+ #  完成超参数网格搜索后的模型
+model_grids={}
 for name,  param in param_grids.items():
-    model_grids[name] = model_selection.GridSearchCV(
-        models[name], param, n_jobs=-1, cv=10, verbose=1)
+    model_grids[name] = model_selection.RandomizedSearchCV(models[name], param, n_jobs=-1, cv=10, verbose=1,scoring='roc_auc')
+    # model_grids[name] = models[name]
 
 # 次级分类器定义
-meta_model = XGBClassifier(n_estimators=2000, max_depth=4, min_child_weight=2, gamma=0.9, subsample=0.8,
-                           colsample_bytree=0.8, objective='binary:logistic', nthread=-1, scale_pos_weight=1)
-
-
+# meta_model =  XGBClassifier(n_estimators=2000, max_depth=4, min_child_weight=2, gamma=0.9, subsample=0.8,  colsample_bytree=0.8, objective='binary:logistic', nthread=-1, scale_pos_weight=1)
+meta_model = LogisticRegression(solver='liblinear')
 def train_model(X, y):
     # SMOTE过采样
-    smo = SMOTE(random_state=42, n_jobs=-1)
+    smo = SMOTE(random_state=42, n_jobs=-1 )
     X_sampling,  y_sampling = smo.fit_resample(X, y)
 
     # K折交叉验证
@@ -100,19 +109,22 @@ def train_model(X, y):
 
         y_hats = []
         # 依次用基学习器进行训练和预测
-        for name,  model_grid in model_grids.items():
+        for name,  model_grid  in model_grids.items():
             # 注意，这里没有实际修改model_grid[name]，
             # 此处是为了得到oof_pred,后面才会用全部数据集训练,
             # 这里每折训练所得的参数不会保存
             model_grid.fit(train_X, train_y)
+            # y_hats.append(model_grid.predict(test_X).reshape(-1, 1))
             y_hats.append(model_grid.predict(test_X).reshape(-1, 1))
             # 评估k折验证过程中初级分类器在训练集上的表现
             acc = accuracy_score(train_y, model_grid.predict(train_X))
             recall = recall_score(train_y, model_grid.predict(train_X))
             preci = precision_score(train_y, model_grid.predict(train_X))
+            auc =roc_auc_score(train_y, model_grid.predict(train_X))
             print(" Sub model %s accuracy: : %.3f " % (name, acc))
             print(" Sub model %s recalll: %.3f " % (name, recall))
             print(" Sub model %s precision: %.3f " % (name, preci))
+            print(" Sub model %s auc: %.3f " % (name, auc))
 
         # 将其输出组成新的特征
         pred = np.concatenate(
@@ -122,21 +134,23 @@ def train_model(X, y):
         idx += 1
 
     # 用所有数据训练基础分类器
-    for name, _ in model_grids.items():
+    for name, _  in model_grids.items():
         # 这里才对model_grids[name]进行实际修改
         model_grids[name].fit(X_sampling, y_sampling)
-        joblib.dump(model_grid, os.path.join(model_root, name + '.json'))
+        joblib.dump(model_grid, os.path.join(model_root, name +'.json'))
 
     # 用所有数据训练次级分类器
-    meta_model. fit(oof_pred, oof_y)
+    meta_model. fit(oof_pred,oof_y)
 
     # 评估次级分类器在训练集上的误差
     acc = accuracy_score(oof_y, meta_model.predict(oof_pred))
     recall = recall_score(oof_y, meta_model.predict(oof_pred))
     preci = precision_score(oof_y, meta_model.predict(oof_pred))
-    print(" Meta model  accuracy: : %.3f " % acc)
+    auc = roc_auc_score(oof_y, meta_model.predict(oof_pred))
+    print(" Meta model  accuracy: : %.3f "  % acc)
     print(" Meta model  recalll: %.3f " % recall)
     print(" Meta model  precision: %.3f " % preci)
+    print(" Meta model  auc: %.3f " % auc)
     joblib.dump(meta_model, os.path.join(model_root, 'meta_model.json'))
 
 
@@ -161,15 +175,18 @@ def evaluate_model(X_test, y_test):
 
     #  基分类器和次级分类器定义
     for name, _ in model_grids.items():
-        model_grids[name] = joblib.load(os.path.join(model_root, name+'.json'))
+        model_grids[name] = joblib.load(os.path.join(model_root, name+'.json')) 
         # 对初级分类器进行评估
         model_grid = model_grids[name]
         acc = accuracy_score(y_test, model_grid.predict(X_test))
         recall = recall_score(y_test, model_grid.predict(X_test))
         preci = precision_score(y_test, model_grid.predict(X_test))
+        auc = roc_auc_score(y_test, model_grid.predict(X_test))
         print(" Sub model %s accuracy: : %.3f " % (name, acc))
         print(" Sub model %s recalll: %.3f " % (name, recall))
         print(" Sub model %s precision: %.3f " % (name, preci))
+        print(" Sub model %s auc: %.3f " % (name, auc))
+
 
     meta_model = joblib.load(os.path.join(model_root, 'meta_model.json'))
     #  对次级分类器进行评估
@@ -177,6 +194,9 @@ def evaluate_model(X_test, y_test):
     acc = accuracy_score(y_test,  y_hat)
     recall = recall_score(y_test, y_hat)
     preci = precision_score(y_test, y_hat)
+    auc = roc_auc_score(y_test, y_hat)
     print('Meta model accuracy: %.3f' % (acc))
     print('Meta model recall: %.3f' % (recall))
     print('Meta model precision %.3f' % (preci))
+    print('Meta model auc  %.3f' % (auc))
+
