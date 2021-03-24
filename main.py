584510f9dbb625177c2fa5869219abd1def0b1ef
lonelyprince7 <1056692290@qq.com>
2021-03-24 10:10:55 +0800

    add

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ Version: 1.0
 Author: ZhangHongYu
 Date: 2021-02-18 13:18:01
 LastEditors: ZhangHongYu
-LastEditTime: 2021-02-28 11:04:20
+LastEditTime: 2021-03-24 09:58:21
 '''
 from feature_eng import read_data
 from feature_eng import data_preprocess
@@ -44,7 +44,7 @@ if __name__ == '__main__':
     X_train, X_test, y_train, y_test = train_test_split(np.array(X), np.array(y), test_size=0.3)
 
     # 采用交叉验证法对初级学习器和次级学习器进行训练
-    #train_model(X_train, y_train)
+    train_model(X_train, y_train)
 
     # 评估学习器的性能
     evaluate_model(X_test, y_test)
